d5681529e74121839c0d3044e3b1bc4330d6c61b
jeevaanandh <anandhjeeva215@gmail.com>
2026-03-26 11:09:52 +0530

    Added linuxWatcher.h file

---
+++ Watchers/linuxWatcher.h
@@ -0,0 +1,11 @@
+//
+// Created by Jeevaanandh Ilayaraja on 26/03/26.
+//
+
+#ifndef WILDFLYWATCHER_LINUXWATCHER_H
+#define WILDFLYWATCHER_LINUXWATCHER_H
+
+
+int watcher(char *projectFolder);
+
+#endif //WILDFLYWATCHER_LINUXWATCHER_H
